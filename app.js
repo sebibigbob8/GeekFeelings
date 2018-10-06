cc4ff90b151ba9e1f42090e98d6531c0f52644e1
sebastien Aubert <sebastien.aubert@heig-vd.ch>
2018-10-06 10:26:13 +0200

    Structure

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/GeekFeelingsTest');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+const User = require('../models/user');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    User.find().sort('name').exec(function(err, users) {
+        if (err) {
+            return next(err);
+        }
+        res.send(users);
+    });
 });
-
 module.exports = router;
