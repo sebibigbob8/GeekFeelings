1941c768133652d27883c3bd8ab3262c9cef0c72
sebastien Aubert <sebastien.aubert@heig-vd.ch>
2018-10-08 14:31:36 +0200

    newBranch

--- models/user.js
+++ models/user.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // Define the schema for users
 const userSchema = new Schema({
-    name: String
+    name: String,
+    username : String,
+    street : String,
+    streetNumber : String,
+    npa : Number,
+    city : String,
+    dateNaissance : Date,
+    description : String,
+    tag : Array,
 });
 // Create the model from the schema and export it
 module.exports = mongoose.model('User', userSchema);
