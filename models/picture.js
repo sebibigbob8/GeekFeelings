da0f03ecf6ed96b0a2412acd8f199da02e61e6ad
Alexandre Kaufmann <alexandre.kaufmann@heig-vd.ch>
2018-10-08 16:33:19 +0200

    Modification picture validator

--- models/picture.js
+++ models/picture.js
@@ -1,8 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // Define the schema for users
-const userSchema = new Schema({
-    name: String
+const pictureSchema = new Schema({
+    src: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 60,
+        unique: true,
+        validate: {
+            validator: validatePictureSrcUniqueness,
+            message: 'Picture {value} already exists'
+        }
+    },
+    description: {
+        type: String,
+        minlength: 0,
+        maxlength: 200,
+    }
+    
 });
+
+function validatePictureUniquess(value, callback){
+    const image = this;
+    this.constructor.findOne().where('src').equals(value).exec(function(err, existingImg) {
+        callback(!err && (!existingImg || existingImg._id.equals(img._id)));
+    });
+}
 // Create the model from the schema and export it
 module.exports = mongoose.model('User', userSchema);
