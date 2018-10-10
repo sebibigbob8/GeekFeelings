d552392d28ddada4e82fb82f6bba4a3eae8dc8a9
sebastien Aubert <sebastien.aubert@heig-vd.ch>
2018-10-10 15:36:21 +0200

    Get user with ID and middleware

--- models/user.js
+++ models/user.js
@@ -10,7 +10,8 @@ const userSchema = new Schema({
     },
     password :  {
         type: String,
-        min: 6
+        min: 6,
+        select: false
     },
     street : String,
     streetNumber : String,
--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,11 @@ var router = express.Router();
 const MONGOOSE = require('mongoose');
 const User = require('../models/user');
 let userModel = MONGOOSE.model('User',User.schema);
-/* GET users listing. */
+const ObjectId = MONGOOSE.Types.ObjectId;
+/**
+ * Get all users
+ */
 router.get('/', function(req, res, next) {
-    /**
-     * Get all users
-     */
     let query = userModel.find({});
     query.exec(function (err,docs)
     {
@@ -19,26 +19,65 @@ router.get('/', function(req, res, next) {
             res.send(docs); //Renvoi des data
         }
     })
+});
+/**
+ * Get a user specified
+ */
+router.get('/:id',loadUserById, function(req, res, next) {
+    res.send(req.user);
+});
+/**
+ * Modify an user
+ */
+router.patch('/:id', function(req, res, next) {
 
 });
-router.get('/:id', function(req, res, next) {
-    /**
-     * Get a user specified
-     */
-});
-router.patch('/:id', function(req, res, next) {
-    /**
-     * Modify an user
-     */
-});
+/**
+ * Create an user
+ */
 router.post('', function(req, res, next) {
-    /**
-     * Create an user
-     */
+
 });
+/**
+ * Delete an user
+ */
 router.delete('/:id', function(req, res, next) {
-    /**
-     * Delete an user
-     */
+
 });
+/**
+ * Load a user in the Request object depending of params given
+ * @param req
+ * @param res
+ * @param next
+ * @returns {*}
+ */
+function loadUserById(req, res, next){
+    const userId = req.params.id;
+    if (!ObjectId.isValid(userId)) {
+        return userNotFound(res, userId);
+    }
+
+    let query = userModel.findById(userId)
+
+    query.exec(function(err, user) {
+        if (err) {
+            console.warn("Could not get the user");
+            next(err); //Fait suivre le message d'erreur
+        } else if (!user) {
+            //TODO
+            //return userNotFound(res, userId);
+        }
+        req.user = user;
+        next();
+    });
+}
+/**
+ * Message in case of an "not found"
+ * @param res
+ * @param userId
+ * @returns {*}
+ */
+function userNotFound(res, userId) {
+    return res.status(404).type('text').send(`No user found with ID ${userId}`);
+}
 module.exports = router;
