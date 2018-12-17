b7bfa680111cd6676bc7d80ef12e362562713d3f
sebastien Aubert <sebastien.aubert@heig-vd.ch>
2018-12-17 11:45:18 +0100

    header TroubleShooting

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
+    res.header("*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
